3180e2266f89bd21cf0375f132a604032a223bb3
Igor Pets <b_ear@mail.ru>
2022-07-31 02:31:58 +0300

    Read options to Pomidoro

--- src/main/java/Pomodoro/PomodoroGame.java
+++ src/main/java/Pomodoro/PomodoroGame.java
@@ -1,7 +1,22 @@
 package Pomodoro;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class PomodoroGame {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        // в секундах
+        int workTime = 10;
+        int breakTime = 2;
+        System.out.println("Добро пожаловать в Помидоро!");
+        String[] userInput = new Scanner(System.in).nextLine().split(" ");
+        //Arrays.stream(userInput).sequential().forEach(msg -> my_print(msg));
+        System.out.println("");
+
+        for (String msg: userInput) {
+            check_comand(msg);
+        }
+    }
+
+    private static void check_comand(String msg) {
+        if (msg != "") System.out.println("Команда:" + msg);
     }
 }
